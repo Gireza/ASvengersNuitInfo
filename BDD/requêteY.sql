f954708fe68c2fc545b0560c3501a20acbebdc06
YvanDesPatates <90316755+YvanDesPatates@users.noreply.github.com>
2021-12-03 04:45:38 +0100

    newrequetes Y

--- BDD/requêteY.sql
+++ BDD/requêteY.sql
@@ -2,4 +2,4 @@
 SELECT nom,prenom FROM Sauveteur ORDER BY nom;
 
 /*commencant par une lettre*/
-SELECT nom,prenom FROM Sauveteur WHERE nom LIKE :lettre+'%' OR prenom LIKE :lettre+'%' ORDER BY nom;
+SELECT nom,prenom FROM Sauveteur WHERE nom LIKE :lettre+'%' OR prenom LIKE :lettre+'%' ORDER BY nom; 
