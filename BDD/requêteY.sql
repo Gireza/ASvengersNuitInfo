0884dd7a9967710753948782f315da030d965350
YvanDesPatates <90316755+YvanDesPatates@users.noreply.github.com>
2021-12-03 04:43:39 +0100

    requêtes
    mes 2 pauv' requetes

--- BDD/requêteY.sql
+++ BDD/requêteY.sql
@@ -1,1 +1,5 @@
-jjj
+/*appuyer sur "sauveteurs"*/
+SELECT nom,prenom FROM Sauveteur ORDER BY nom;
+
+/*commencant par une lettre*/
+SELECT nom,prenom FROM Sauveteur WHERE nom LIKE :lettre+'%' OR prenom LIKE :lettre+'%' ORDER BY nom;
